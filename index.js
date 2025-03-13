72779208ed72f72af53342476dfdbba534b4a887
Mashal <zyadomar112@gmail.com>
2025-03-14 00:37:13 +0200

    delete filter

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { AuthRoutes, ApplicationRoutes, FeedbackRoutes, JobPostRoutes, searchRoute, filterRoute } = require("./Routes/routes");
+const { AuthRoutes, ApplicationRoutes, FeedbackRoutes, JobPostRoutes, searchRoute } = require("./Routes/routes");
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
@@ -19,7 +19,6 @@ app.use(ApplicationRoutes);
 app.use(FeedbackRoutes);
 app.use(JobPostRoutes);
 app.use(searchRoute)
-app.use(filterRoute)
 
 
 const port = process.env.PORT;
