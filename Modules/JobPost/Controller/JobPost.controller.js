594c3dd4ee2bf7250990f0a4b586fd892997e831
Mashal <zyadomar112@gmail.com>
2025-04-22 21:48:59 +0200

    update job list and job post

--- Modules/JobPost/Controller/JobPost.controller.js
+++ Modules/JobPost/Controller/JobPost.controller.js
@@ -37,6 +37,33 @@ const ViewPaginatedJobs = async (req, res) => {
     }
 };
 
+const CreateJob = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { CompanyName, jobTitle, jobType, country, city, area, salary, jobDescription, jobRequirements, requiredSkills, Document } = req.body;
+        const newJopPost = await JobPost.create({
+            createdBy: id,
+            CompanyName,
+            jobTitle,
+            jobType,
+            country,
+            city,
+            area,
+            salary,
+            jobDescription,
+            jobRequirements,
+            requiredSkills,
+            Document
+        });
+        const jopPost = await JobPost.findById(newJopPost._id).populate({ path: "createdBy", select: "email" });
+        return res.status(201).json({ massage: "file uploaded successfully", job: jopPost });
+
+    } catch (err) {
+        return res.status(401).json({ message: "something went wrong", err: err.message });
+    }
+}
+
+
 const getJobById = async (req, res) => {
     try {
         const id = req.params.id;
