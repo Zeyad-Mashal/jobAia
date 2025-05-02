bcb01cb9a9a3f7fc6a0d728dd8f9afa806186fed
Mashal <zyadomar112@gmail.com>
2025-05-02 14:36:16 +0300

    update1

--- Modules/JobPost/JobPost.routes.js
+++ Modules/JobPost/JobPost.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { ViewJobs, CreateJob, ViewPaginatedJobs, getJobById, newestJobs, getRelatedJobs } = require("../JobPost/Controller/JobPost.controller");
+const { ViewJobs, CreateJob, ViewPaginatedJobs, getJobById, newestJobs } = require("../JobPost/Controller/JobPost.controller");
 
 
 router.get('/JobPosting', ViewJobs);
