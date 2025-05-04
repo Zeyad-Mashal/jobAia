b9566ebaf8bbca1020ffae85d5a5aca2d7f73a7a
Mashal <zyadomar112@gmail.com>
2025-05-04 23:32:15 +0300

    update

--- Modules/Profile/Profile.routes.js
+++ Modules/Profile/Profile.routes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
+const { Postdetails, getProfile, updateDetails, getUserApplications, getUserApplicants } = require("./Controller/Profile.controller");
 
-// استخدم المسار الكامل مع .js
-const profileController = require(path.join(__dirname, 'Controller', 'Profile.controller.js'));
 
-router.post("/profile/:id", profileController.Postdetails);
-router.get("/profile/:id/:idSkill", profileController.getProfile);
-router.put("/profile/:id", profileController.updateDetails);
-router.get("/applications/user/:id", profileController.getUserApplications);
-router.get("/applications/JobPost/:jobPostId", profileController.getUserApplicants);
+router.post("/profile/:id", Postdetails);
+
+router.get("/profile/:id/:idSkill", getProfile);
+
+router.put("/profile/:id", updateDetails);
+
+router.get("/applications/user/:id", getUserApplications);
+
+router.get("/applications/JobPost/:jobPostId", getUserApplicants)
+
 
 module.exports = router;
