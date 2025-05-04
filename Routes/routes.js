f96895bb56080e0bd2f65a58e8321804901f2ecd
Mashal <zyadomar112@gmail.com>
2025-05-04 23:40:33 +0300

    profile

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,7 +7,7 @@ const FeedbackRoutes = require("../Modules/Feedback/Feedback.routes");
 const JobPostRoutes = require("../Modules/JobPost/JobPost.routes");
 const searchRoute = require("../Modules/search/search.routes");
 const filterRoute = require("../Modules/filter/filter.routes");
-// const ProfileRoutes = require("../Modules/Profile/Profile.routes");
+const ProfileRoutes = require("../Modules/Profile/Profile.routes");
 
 router.use(AuthRoutes);
 router.use(ApplicationRoutes);
@@ -15,6 +15,6 @@ router.use(FeedbackRoutes);
 router.use(JobPostRoutes);
 router.use(searchRoute);
 router.use(filterRoute);
-// router.use(ProfileRoutes);
+router.use(ProfileRoutes);
 
 module.exports = router;
