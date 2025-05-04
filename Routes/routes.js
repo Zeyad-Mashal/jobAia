9020620d7c229974b76c5e541052c35fcec2b67e
Mashal <zyadomar112@gmail.com>
2025-05-05 00:53:28 +0300

    update

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const router = express.Router();
-
 const AuthRoutes = require("../Modules/Auth/Auth.routes");
+
 const ApplicationRoutes = require("../Modules/Application/Application.routes");
+
 const FeedbackRoutes = require("../Modules/Feedback/Feedback.routes");
+
 const JobPostRoutes = require("../Modules/JobPost/JobPost.routes");
-const searchRoute = require("../Modules/search/search.routes");
-const filterRoute = require("../Modules/filter/filter.routes");
 
-router.use(AuthRoutes);
-router.use(ApplicationRoutes);
-router.use(FeedbackRoutes);
-router.use(JobPostRoutes);
-router.use(searchRoute);
-router.use(filterRoute);
+const searchRoute = require("../Modules/search/search.routes")
 
-module.exports = router;
+const filterRoute = require("../Modules/filter/filter.routes")
+
+
+
+module.exports = {
+    AuthRoutes,
+    ApplicationRoutes,
+    FeedbackRoutes,
+    JobPostRoutes,
+    searchRoute,
+    filterRoute,
+} 
